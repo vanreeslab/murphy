270a3ce4d03575e01cc9280ac22daeb75ca532a7
Thomas_Gillis <thomas.gillis@uclouvain.be>
2020-04-08 23:30:39 -0400

    :green_heart: enables a stupid mpi strategy for the test

--- test/src/main_test.cpp
+++ test/src/main_test.cpp
@@ -4,18 +4,33 @@
 #include "murphy.hpp"
 
 int main(int argc, char** argv) {
+    // we should first init Google Test and then MPI.
+    for(int ia=0; ia<argc; ia++){
+            printf("command = %s\n",argv[ia]);
+        }
+    // but I need the rank to init google test, so I do the opposite
+    // init MPI and pe4st
     MPI_Init(&argc, &argv);
     MPI_Comm comm = MPI_COMM_WORLD;
     sc_init(comm, 1, 1, NULL, SC_LP_ESSENTIAL);
     p4est_init(NULL, SC_LP_PRODUCTION);
     //-------------------------------------------------------------------------
     {
-        int comm_size;
-        MPI_Comm_size(MPI_COMM_WORLD,&comm_size);
-        if(comm_size != 1){
-            m_log("unable to run tests with more than 1 CPU (for the moment)");
-            MPI_Abort(MPI_COMM_WORLD,MPI_ERR_ASSERT);
+        int rank;
+        MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+        for(int ia=0; ia<argc; ia++){
+            int comp = strcmp(argv[ia],"--gtest_output=xml:report_test.xml");
+            if(comp==0){
+                sprintf(argv[ia],"--gtest_output=xml:report_test_rank%d.xml",rank);
+            }
+
+            int comp = strcmp(argv[ia],"--gtest_output=xml:report_valid.xml");
+            if(comp==0){
+                sprintf(argv[ia],"--gtest_output=xml:report_valid_rank%d.xml",rank);
+            }
         }
+        // init google and remove Google Test arguments
         ::testing::InitGoogleTest(&argc, argv);
         return RUN_ALL_TESTS();
     }
