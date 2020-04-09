c47a4e76ec1c01d4f585ff275bbdfc711ca98bd8
Thomas_Gillis <thomas.gillis@uclouvain.be>
2020-04-08 23:37:41 -0400

    :bug: :green_heart: corrects some mpi-related bugs in main_test

--- test/src/main_test.cpp
+++ test/src/main_test.cpp
@@ -5,9 +5,9 @@
 
 int main(int argc, char** argv) {
     // we should first init Google Test and then MPI.
-    for(int ia=0; ia<argc; ia++){
-            printf("command = %s\n",argv[ia]);
-        }
+    for (int ia = 0; ia < argc; ia++) {
+        printf("command = %s\n", argv[ia]);
+    }
     // but I need the rank to init google test, so I do the opposite
     // init MPI and pe4st
     MPI_Init(&argc, &argv);
@@ -15,26 +15,30 @@ int main(int argc, char** argv) {
     sc_init(comm, 1, 1, NULL, SC_LP_ESSENTIAL);
     p4est_init(NULL, SC_LP_PRODUCTION);
     //-------------------------------------------------------------------------
+    int err;
     {
         int rank;
-        MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-        for(int ia=0; ia<argc; ia++){
-            int comp = strcmp(argv[ia],"--gtest_output=xml:report_test.xml");
-            if(comp==0){
-                sprintf(argv[ia],"--gtest_output=xml:report_test_rank%d.xml",rank);
+        for (int ia = 0; ia < argc; ia++) {
+            int comp;
+            comp = strcmp(argv[ia], "--gtest_output=xml:report_test.xml");
+            if (comp == 0) {
+                sprintf(argv[ia], "--gtest_output=xml:report_test_rank%d.xml", rank);
             }
 
-            int comp = strcmp(argv[ia],"--gtest_output=xml:report_valid.xml");
-            if(comp==0){
-                sprintf(argv[ia],"--gtest_output=xml:report_valid_rank%d.xml",rank);
+            comp = strcmp(argv[ia], "--gtest_output=xml:report_valid.xml");
+            if (comp == 0) {
+                sprintf(argv[ia], "--gtest_output=xml:report_valid_rank%d.xml", rank);
             }
         }
         // init google and remove Google Test arguments
         ::testing::InitGoogleTest(&argc, argv);
-        return RUN_ALL_TESTS();
+        err = RUN_ALL_TESTS();
     }
     //-------------------------------------------------------------------------
     sc_finalize();
     MPI_Finalize();
+
+    return err;
 }
