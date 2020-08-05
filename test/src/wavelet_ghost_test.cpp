fc09014aad91ee9de32c69eb8cfdd6a3e963b100
Thomas_Gillis <thomas.gillis@uclouvain.be>
2020-08-04 22:03:30 -0400

    :bug: :white_check_mark: discard the physical bc while checking the error + try to solve mpi problem in docker

--- test/src/wavelet_ghost_test.cpp
+++ test/src/wavelet_ghost_test.cpp
@@ -11,55 +11,56 @@
 #include "subblock.hpp"
 #include "wavelet.hpp"
 
-#define DOUBLE_TOL 1e-12
+#define DOUBLE_TOL 1e-11
 
 using std::list;
 
-// // define a mask on the edge and corner ghosts
-// class MaskPhysBC : public OperatorF {
-//    protected:
-//     real_t L_[3];
-//     void   ApplyOpF(const qid_t* qid, GridBlock* block, Field* fid) override {
-//         //-------------------------------------------------------------------------
-//         real_t pos[3];
+// define a mask on the edge and corner ghosts
+class MaskPhysBC : public OperatorF {
+   protected:
+    real_t L_[3];
+    void   ApplyOpF(const qid_t* qid, GridBlock* block, Field* fid) override {
+        //-------------------------------------------------------------------------
+        real_t pos[3];
 
-//         for (lda_t ida = 0; ida < fid->lda(); ida++) {
-//             data_ptr data = block->data(fid, ida);
-//             for (int i2 = (-M_GS); i2 < (M_N + M_GS); i2++) {
-//                 for (int i1 = (-M_GS); i1 < (M_N + M_GS); i1++) {
-//                     for (int i0 = (-M_GS); i0 < (M_N + M_GS); i0++) {
-//                         // get the position
-//                         real_t pos[3];
-//                         m_pos(pos, i0, i1, i2, block->hgrid(), block->xyz());
+        for (lda_t ida = 0; ida < fid->lda(); ida++) {
+            data_ptr data = block->data(fid, ida);
+            for (int i2 = (-M_GS); i2 < (M_N + M_GS); i2++) {
+                for (int i1 = (-M_GS); i1 < (M_N + M_GS); i1++) {
+                    for (int i0 = (-M_GS); i0 < (M_N + M_GS); i0++) {
+                        // get the position
+                        real_t pos[3];
+                        m_pos(pos, i0, i1, i2, block->hgrid(), block->xyz());
 
-//                         // const bool is_out_0 = (pos[0] < 0.0) || (pos[0] >= L_[0]);
-//                         // const bool is_out_1 = (pos[1] < 0.0) || (pos[1] >= L_[1]);
-//                         // const bool is_out_2 = (pos[2] < 0.0) || (pos[2] >= L_[2]);
-//                         const bool is_out_0 = (pos[0] < 0.0) || (pos[0] >= L_[0]);
-//                         const bool is_out_1 = (pos[1] < 0.0) || (pos[1] >= L_[0]);
-//                         const bool is_out_2 = (pos[2] < 0.0) || (pos[2] >= L_[0]);
+                        const bool is_out_0 = (pos[0] < 0.0) || (pos[0] >= L_[0]);
+                        const bool is_out_1 = (pos[1] < 0.0) || (pos[1] >= L_[1]);
+                        const bool is_out_2 = (pos[2] < 0.0) || (pos[2] >= L_[2]);
 
-//                         const bool to_trash = ((is_out_0 + is_out_1 + is_out_2) > 0);
+                        // const bool too_big_0 = (i0 < 0) || (i0 >= M_N);
+                        // const bool too_big_1 = (i1 < 0) || (i1 >= M_N);
+                        // const bool too_big_2 = (i2 < 0) || (i2 >= M_N);
 
-//                         // data[m_idx(i0, i1, i2)] = data[m_idx(i0, i1, i2)] * (!to_trash);
-//                     }
-//                 }
-//             }
-//         }
-//         //-------------------------------------------------------------------------
-//     };
+                        const bool to_trash = ((is_out_0 + is_out_1 + is_out_2) >= 1);
 
-//    public:
-//     MaskPhysBC(const lid_t L[3]) {
-//         m_begin;
-//         //-------------------------------------------------------------------------
-//         for (lda_t id = 0; id < 3; ++id) {
-//             L_[id] = (real_t)L[id];
-//         }
-//         //-------------------------------------------------------------------------
-//         m_end;
-//     };
-// };
+                        data[m_idx(i0, i1, i2)] = data[m_idx(i0, i1, i2)] * (!to_trash);
+                    }
+                }
+            }
+        }
+        //-------------------------------------------------------------------------
+    };
+
+   public:
+    MaskPhysBC(const lid_t L[3]) {
+        m_begin;
+        //-------------------------------------------------------------------------
+        for (lda_t id = 0; id < 3; ++id) {
+            L_[id] = (real_t)L[id];
+        }
+        //-------------------------------------------------------------------------
+        m_end;
+    };
+};
 
 class test_Wavelet_Ghost : public ::testing::Test {
    protected:
@@ -69,7 +70,7 @@ class test_Wavelet_Ghost : public ::testing::Test {
 
 //==============================================================================================================================
 #if (WAVELET_N == 2)
-TEST_F(test_Wavelet_Ghost, ghost_order_2_2) {
+TEST_F(test_Wavelet_Ghost, ghost_order_2_x) {
     for (lda_t id = 0; id < 3; id++) {
         bool  period[3] = {false, false, false};
         lid_t L[3]      = {1, 1, 1};
@@ -108,22 +109,23 @@ TEST_F(test_Wavelet_Ghost, ghost_order_2_2) {
         SetPolynom field_sol(deg, dir, true);
         field_sol(&grid, &sol);
 
-        // // mask both the sol and the result
-        // MaskPhysBC mask(L);
-        // mask(&grid, &test);
-        // mask(&grid, &sol);
+        // mask both the sol and the result
+        MaskPhysBC mask(L);
+        mask(&grid, &test);
+        mask(&grid, &sol);
 
         // now, we need to check
         real_t          norm2, normi;
         ErrorCalculator error(&grid);
         error.Norms(&grid, &test, &sol, &norm2, &normi);
 
-        ASSERT_NEAR(normi, 0.0, DOUBLE_TOL) << "the inf-norm is " << normi << " in id = " << id;
-        ASSERT_NEAR(norm2, 0.0, DOUBLE_TOL) << "the 2-norm is " << norm2 << " in id = " << id;
+        m_log("checking in dim %d: the two norms: %e %e", id, normi, norm2);
+        ASSERT_NEAR(normi, 0.0, DOUBLE_TOL);
+        ASSERT_NEAR(norm2, 0.0, DOUBLE_TOL);
     }
 }
 #elif (WAVELET_N == 4)
-TEST_F(test_Wavelet_Ghost, ghost_order_4_2) {
+TEST_F(test_Wavelet_Ghost, ghost_order_4_x) {
     for (lda_t id = 0; id < 3; id++) {
         bool  period[3] = {false, false, false};
         lid_t L[3]      = {1, 1, 1};
@@ -140,7 +142,7 @@ TEST_F(test_Wavelet_Ghost, ghost_order_4_2) {
 
         Field test("test", 1);
         grid.AddField(&test);
-        test.bctype(M_BC_EXTRAP_4);
+        test.bctype(M_BC_EXTRAP_5);
 
         // create the initial field
         const lid_t  deg[3] = {3, 3, 3};
@@ -162,18 +164,19 @@ TEST_F(test_Wavelet_Ghost, ghost_order_4_2) {
         SetPolynom field_sol(deg, dir, true);
         field_sol(&grid, &sol);
 
-        // // mask both the sol and the result
-        // MaskPhysBC mask(L);
-        // mask(&grid, &test);
-        // mask(&grid, &sol);
+        // mask both the sol and the result
+        MaskPhysBC mask(L);
+        mask(&grid, &test);
+        mask(&grid, &sol);
 
         // now, we need to check
         real_t          norm2, normi;
         ErrorCalculator error(&grid);
         error.Norms(&grid, &test, &sol, &norm2, &normi);
 
-        ASSERT_NEAR(normi, 0.0, DOUBLE_TOL) << "the inf-norm is " << normi << " in id = " << id;
-        ASSERT_NEAR(norm2, 0.0, DOUBLE_TOL) << "the 2-norm is " << norm2 << " in id = " << id;
+        m_log("checking in dim %d: the two norms: %e %e", id, normi, norm2);
+        ASSERT_NEAR(normi, 0.0, DOUBLE_TOL);
+        ASSERT_NEAR(norm2, 0.0, DOUBLE_TOL);
     }
 }
 #endif
