092919b7b6728c5549f83d71094786c78f2e9446
Thomas_Gillis <thomas.gillis@uclouvain.be>
2020-10-14 21:29:41 -0400

    :bug: solve a bug in the tests + add a base level

--- test/src/valid_wavelet_convergence.cpp
+++ test/src/valid_wavelet_convergence.cpp
@@ -11,7 +11,8 @@
 #include "subblock.hpp"
 #include "wavelet.hpp"
 
-#define DOUBLE_TOL 1e-11
+#define ORDER_TOL 0.3
+#define BLVL 2
 
 using std::list;
 
@@ -23,24 +24,23 @@ class valid_Wavelet_Convergence : public ::testing::Test {
 
 //==============================================================================================================================
 TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_periodic_sin) {
+    // init the errors
+    real_t erri[2];
+    real_t err2[2];
     for (lda_t id = 0; id < 3; id++) {
-        // init the errors
-        real_t erri[3] = {0.0, 0.0, 0.0};
-        real_t err2[3] = {0.0, 0.0, 0.0};
-
         // setup the mesh
         bool  period[3] = {true, true, true};
         lid_t L[3]      = {1, 1, 1};
         L[id]           = 3;
 
-        for (level_t il = 2; il < 4; ++il) {
-            Grid grid(il, period, L, MPI_COMM_WORLD, nullptr);
+        for (level_t il = 0; il < 1; ++il) {
+            Grid grid(il + BLVL, period, L, MPI_COMM_WORLD, nullptr);
 
             // create the patch refinement to refine the middle tree
             real_t origin[3]      = {0.0, 0.0, 0.0};
             origin[id]            = 1;
             real_t      length[3] = {1.0, 1.0, 1.0};
-            Patch       p1(origin, length, il + 1);
+            Patch       p1(origin, length, il + BLVL + 1);
             list<Patch> patch{p1};
             grid.Adapt(&patch);
 
@@ -74,36 +74,35 @@ TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_periodic_sin) {
             ErrorCalculator error(grid.interp());
             error.Norms(&grid, &test, &sol, err2 + il, erri + il);
 
-            m_log("checking in dim %d: res = %f, ei = %e e2 = %e", id, std::pow(2, il), erri[il], err2[il]);
+            m_log("checking in dim %d: res = %f, ei = %e e2 = %e", id, std::pow(2, il + BLVL), erri[il], err2[il]);
         }
-        real_t conv2 = -log(err2[2] / err2[1]) / log(2);
-        real_t convi = -log(erri[2] / erri[1]) / log(2);
+        real_t conv2 = -log(err2[1] / err2[0]) / log(2);
+        real_t convi = -log(erri[1] / erri[0]) / log(2);
 
         m_log("==> the convergence orders are: norm_2:%e norm_i:%e", conv2, convi);
-        ASSERT_GE(conv2, M_WAVELET_N - 0.1);
-        ASSERT_GE(convi, M_WAVELET_N - 0.1);
+        ASSERT_GE(conv2, M_WAVELET_N - ORDER_TOL);
+        ASSERT_GE(convi, M_WAVELET_N - ORDER_TOL);
     }
 }
 //==============================================================================================================================
 TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_periodic_cos) {
+    // init the errors
+    real_t erri[2];
+    real_t err2[2];
     for (lda_t id = 0; id < 3; id++) {
-        // init the errors
-        real_t erri[3] = {0.0, 0.0, 0.0};
-        real_t err2[3] = {0.0, 0.0, 0.0};
-
         // setup the mesh
         bool  period[3] = {true, true, true};
         lid_t L[3]      = {1, 1, 1};
         L[id]           = 3;
 
-        for (level_t il = 2; il < 4; ++il) {
-            Grid grid(il, period, L, MPI_COMM_WORLD, nullptr);
+        for (level_t il = 0; il < 2; ++il) {
+            Grid grid(il + BLVL, period, L, MPI_COMM_WORLD, nullptr);
 
             // create the patch refinement to refine the middle tree
             real_t origin[3]      = {0.0, 0.0, 0.0};
             origin[id]            = 1;
             real_t      length[3] = {1.0, 1.0, 1.0};
-            Patch       p1(origin, length, il + 1);
+            Patch       p1(origin, length, il + BLVL + 1);
             list<Patch> patch{p1};
             grid.Adapt(&patch);
 
@@ -137,33 +136,34 @@ TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_periodic_cos) {
             ErrorCalculator error(grid.interp());
             error.Norms(&grid, &test, &sol, err2 + il, erri + il);
 
-            m_log("checking in dim %d: res = %f, ei = %e e2 = %e", id, std::pow(2, il), erri[il], err2[il]);
+            m_log("checking in dim %d: res = %f, ei = %e e2 = %e", id, std::pow(2, il + BLVL), erri[il], err2[il]);
         }
-        real_t conv2 = -log(err2[2] / err2[1]) / log(2);
-        real_t convi = -log(erri[2] / erri[1]) / log(2);
+        real_t conv2 = -log(err2[1] / err2[0]) / log(2);
+        real_t convi = -log(erri[1] / erri[0]) / log(2);
 
-        m_log("the convergence orders are: norm_2:%e norm_i:%e", conv2, convi);
-        ASSERT_GE(conv2, M_WAVELET_N - 0.1);
-        ASSERT_GE(convi, M_WAVELET_N - 0.1);
+        m_log("==> the convergence orders are: norm_2:%e norm_i:%e", conv2, convi);
+        ASSERT_GE(conv2, M_WAVELET_N - ORDER_TOL);
+        ASSERT_GE(convi, M_WAVELET_N - ORDER_TOL);
     }
 }
 
 //==============================================================================================================================
-TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_extrap_polynom) {
-    real_t ei[2];
-    real_t e2[2];
+TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_extrap_cos) {
+    // init the errors
+    real_t erri[2];
+    real_t err2[2];
     for (lda_t id = 0; id < 3; id++) {
-        for (level_t il = 2; il < 4; il++) {
+        for (level_t il = 0; il < 2; ++il) {
             bool  period[3] = {false, false, false};
             lid_t L[3]      = {1, 1, 1};
             L[id]           = 3;
-            Grid grid(il, period, L, MPI_COMM_WORLD, nullptr);
+            Grid grid(il + BLVL, period, L, MPI_COMM_WORLD, nullptr);
 
             // create the patch refinement to refine the middle tree
             real_t origin[3]      = {0.0, 0.0, 0.0};
             origin[id]            = 1.0;
             real_t      length[3] = {1.0, 1.0, 1.0};
-            Patch       p1(origin, length, il + 1);
+            Patch       p1(origin, length, il + BLVL + 1);
             list<Patch> patch{p1};
             grid.Adapt(&patch);
 
@@ -171,11 +171,10 @@ TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_extrap_polynom) {
             grid.AddField(&test);
             test.bctype(M_BC_EXTRAP);
 
-            // create the initial field
-            const lid_t  deg[3]   = {M_WAVELET_N + 2, M_WAVELET_N + 1, M_WAVELET_N};
-            const real_t dir[3]   = {M_SQRT2, -M_PI, M_E};
-            const real_t shift[3] = {0.0, 0.0, 0.0};
-            SetPolynom   field_init(deg, dir, shift);
+            const real_t sin_len[3] = {(real_t)L[0], (real_t)L[1], (real_t)L[2]};
+            const real_t freq[3]    = {2.0, 2.0, 2.0};
+            const real_t alpha[3]   = {1.0, 1.0, 1.0};
+            SetCosinus     field_init(sin_len, freq, alpha);
             field_init(&grid, &test);
 
             // pull the ghosts
@@ -189,7 +188,7 @@ TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_extrap_polynom) {
             // create the solution field
             Field sol("sol", 1);
             grid.AddField(&sol);
-            SetPolynom field_sol(deg, dir, shift, grid.interp());
+            SetCosinus field_sol(sin_len, freq, alpha, grid.interp());
             field_sol(&grid, &sol);
 
             // mask both the sol and the result
@@ -200,37 +199,36 @@ TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_extrap_polynom) {
             // now, we need to check
             real_t          norm2, normi;
             ErrorCalculator error(grid.interp());
-            error.Norms(&grid, &test, &sol, e2 + il, ei + il);
+            error.Norms(&grid, &test, &sol, err2 + il, erri + il);
 
-            m_log("checking in dim %d, level = %d: the two norms: %e %e", id, il, ei[il], e2[il]);
+            m_log("checking in dim %d: res = %f, ei = %e e2 = %e", id, std::pow(2, il + BLVL), erri[il], err2[il]);
         }
+        real_t conv2 = -log(err2[1] / err2[0]) / log(2);
+        real_t convi = -log(erri[1] / erri[0]) / log(2);
 
-        real_t conv2 = -log(e2[2] / e2[1]) / log(2);
-        real_t convi = -log(ei[2] / ei[1]) / log(2);
         m_log("==> the convergence orders are: norm_2:%e norm_i:%e", conv2, convi);
-
-        ASSERT_GE(convi, M_WAVELET_N - 0.1);
-        ASSERT_GE(conv2, M_WAVELET_N - 0.1);
+        ASSERT_GE(conv2, M_WAVELET_N - ORDER_TOL);
+        ASSERT_GE(convi, M_WAVELET_N - ORDER_TOL);
     }
 }
 
 //==============================================================================================================================
 TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_dirichlet0_polynom) {
-    real_t ei[2];
-    real_t e2[2];
+    real_t erri[2];
+    real_t err2[2];
     for (lda_t id = 0; id < 3; id++) {
-        for (level_t il = 2; il < 4; il++) {
+        for (level_t il = 0; il < 2; il++) {
             bool period[3] = {true, true, true};
             period[id]     = false;
             lid_t L[3]     = {1, 1, 1};
             L[id]          = 3;
-            Grid grid(il, period, L, MPI_COMM_WORLD, nullptr);
+            Grid grid(il + BLVL, period, L, MPI_COMM_WORLD, nullptr);
 
             // create the patch refinement to refine the middle tree
             real_t origin[3]      = {0.0, 0.0, 0.0};
             origin[id]            = 1.0;
             real_t      length[3] = {1.0, 1.0, 1.0};
-            Patch       p1(origin, length, il + 1);
+            Patch       p1(origin, length, il + BLVL + 1);
             list<Patch> patch{p1};
             grid.Adapt(&patch);
 
@@ -275,37 +273,36 @@ TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_dirichlet0_polynom
             // now, we need to check
             real_t          norm2, normi;
             ErrorCalculator error(grid.interp());
-            error.Norms(&grid, &test, &sol, e2 + il, ei + il);
+            error.Norms(&grid, &test, &sol, err2 + il, erri + il);
 
-            m_log("checking in dim %d, level = %d: the two norms: %e %e", id, il, ei[il], e2[il]);
+            m_log("checking in dim %d: res = %f, ei = %e e2 = %e", id, std::pow(2, il + BLVL), erri[il], err2[il]);
         }
+        real_t conv2 = -log(err2[1] / err2[0]) / log(2);
+        real_t convi = -log(erri[1] / erri[0]) / log(2);
 
-        real_t conv2 = -log(e2[2] / e2[1]) / log(2);
-        real_t convi = -log(ei[2] / ei[1]) / log(2);
         m_log("==> the convergence orders are: norm_2:%e norm_i:%e", conv2, convi);
-
-        ASSERT_GE(convi, M_WAVELET_N - 0.1);
-        ASSERT_GE(conv2, M_WAVELET_N - 0.1);
+        ASSERT_GE(conv2, M_WAVELET_N - ORDER_TOL);
+        ASSERT_GE(convi, M_WAVELET_N - ORDER_TOL);
     }
 }
 
 //==============================================================================================================================
 TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_neuman0_cos) {
-    real_t ei[2];
-    real_t e2[2];
+    real_t erri[2];
+    real_t err2[2];
     for (lda_t id = 0; id < 3; id++) {
-        for (level_t il = 2; il < 4; il++) {
+        for (level_t il = 0; il < 2; il++) {
             bool period[3] = {true, true, true};
             period[id]     = false;
             lid_t L[3]     = {1, 1, 1};
             L[id]          = 3;
-            Grid grid(il, period, L, MPI_COMM_WORLD, nullptr);
+            Grid grid(il + BLVL, period, L, MPI_COMM_WORLD, nullptr);
 
             // create the patch refinement to refine the middle tree
             real_t origin[3]      = {0.0, 0.0, 0.0};
             origin[id]            = 1.0;
             real_t      length[3] = {1.0, 1.0, 1.0};
-            Patch       p1(origin, length, il + 1);
+            Patch       p1(origin, length, il + BLVL + 1);
             list<Patch> patch{p1};
             grid.Adapt(&patch);
 
@@ -348,16 +345,15 @@ TEST_F(valid_Wavelet_Convergence, ghost_reconstruction_perper_neuman0_cos) {
             // now, we need to check
             real_t          norm2, normi;
             ErrorCalculator error(grid.interp());
-            error.Norms(&grid, &test, &sol, e2 + il, ei + il);
+            error.Norms(&grid, &test, &sol, err2 + il, erri + il);
 
-            m_log("checking in dim %d, level = %d: the two norms: %e %e", id, il, ei[il], e2[il]);
+            m_log("checking in dim %d: res = %f, ei = %e e2 = %e", id, std::pow(2, il + BLVL), erri[il], err2[il]);
         }
+        real_t conv2 = -log(err2[1] / err2[0]) / log(2);
+        real_t convi = -log(erri[1] / erri[0]) / log(2);
 
-        real_t conv2 = -log(e2[2] / e2[1]) / log(2);
-        real_t convi = -log(ei[2] / ei[1]) / log(2);
         m_log("==> the convergence orders are: norm_2:%e norm_i:%e", conv2, convi);
-
-        ASSERT_GE(convi, M_WAVELET_N - 0.1);
-        ASSERT_GE(conv2, M_WAVELET_N - 0.1);
+        ASSERT_GE(conv2, M_WAVELET_N - ORDER_TOL);
+        ASSERT_GE(convi, M_WAVELET_N - ORDER_TOL);
     }
 }
