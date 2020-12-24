05ae27971894bc08a955cc8aa7096911c2c20fb3
Thomas Gillis (thomas.gillis@uclouvain.be) <tgillis@mit.edu>
2020-12-24 07:22:36 +0100

    :rocket: add the weak scalability test scripts for nic5

--- run/run_nic5.sh
+++ run/run_nic5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Submission script for Lemaitre3 
-#SBATCH --job-name=murphy_scalability
+#SBATCH --job-name=weak-scalability
 #SBATCH --time=01:00:00
 #
-#SBATCH --ntasks=256
+##SBATCH --ntasks=64
 #SBATCH --ntasks-per-node=64
 #SBATCH --mem-per-cpu=2625
 # ##SBATCH --partition=batch,debug 
@@ -13,7 +13,7 @@
 
 HOME_MURPHY=/home/ucl/tfl/tgillis/murphy
 SCRATCH=$GLOBALSCRATCH
-RUN_DIR=murphy_$SLURM_JOB_ID
+RUN_DIR=murphy_weak_${SLURM_JOB_NUM_NODES}_${SLURM_JOB_ID}
 
 # create the tmp directory
 mkdir -p $SCRATCH/$RUN_DIR
@@ -27,5 +27,6 @@ cp -r $HOME_MURPHY/murphy $SCRATCH/$RUN_DIR
 cd $SCRATCH/$RUN_DIR 
 
 # run that shit
-mpirun ./murphy --ns --profile --iter-max=100 > $SLURM_JOB_ID.log
+#mpirun ./murphy --abc --profile --iter-max=500 --iter-dump=10 --dump-detail=1 --rtol=1e-2 --ctol=1e-4 --vr-sigma=0.025 --dom=1,1,$SLURM_JOB_NUM_NODES > log_$SLURM_JOB_ID.log
+mpirun ./murphy --abc --profile --iter-max=100 --iter-dump=1000 --rtol=1e-2 --ctol=1e-4 --vr-sigma=0.01 --dom=1,1,$SLURM_JOB_NUM_NODES > log_$SLURM_JOB_ID.log
 #mpirun -d valgrind --error-limit=no --suppressions=${EBROOTOPENMPI}/share/openmpi/openmpi-valgrind.supp ./murphy --ns --profile --iter-max=10 > $SLURM_JOB_ID.log
