8bc27b44578b778bfc49ac242ff9ae065b986494
Thomas_Gillis <thomas.gillis@uclouvain.be>
2021-05-12 15:20:43 -0400

    :bug: change the default params in the epsilon test

--- src/clients/epsilon_test.cpp
+++ src/clients/epsilon_test.cpp
@@ -115,7 +115,7 @@ void EpsilonTest::InitParam(ParserArguments* param) {
 
 void EpsilonTest::Run() {
     //-------------------------------------------------------------------------
-    real_t depsilon = 1e-10;
+    real_t depsilon = delta_eps_;
     real_t epsilon  = eps_start_;
     m_log("starting with epsilon = %e", epsilon);
     while (epsilon >= std::pow(2.0, -26)) {
