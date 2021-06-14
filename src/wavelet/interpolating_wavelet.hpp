417e05cf5eb882b8948f0901719b04d07254ddfc
Thomas_Gillis <thomas.gillis@uclouvain.be>
2021-06-14 13:48:27 -0400

    :bug: inline the template specialization

--- src/wavelet/interpolating_wavelet.hpp
+++ src/wavelet/interpolating_wavelet.hpp
@@ -8,17 +8,17 @@
 #include "wavelet.hpp"
 
 template <lda_t N, lda_t NT>
-constexpr short_t len_ha_ = 0;  //!< length of the Ha filter
+static constexpr short_t len_ha_ = 0;  //!< length of the Ha filter
 template <lda_t N, lda_t NT>
-constexpr short_t len_ga_ = 0;  //!< length of the S dual filter
+static constexpr short_t len_ga_ = 0;  //!< length of the S dual filter
 template <lda_t N, lda_t NT>
-constexpr short_t len_js_ = 0;  //!< length of the Js filter
+static constexpr short_t len_js_ = 0;  //!< length of the Js filter
 template <lda_t N, lda_t NT>
-constexpr short_t len_ks_ = 0;  //!< length of the Ks filter
+static constexpr short_t len_ks_ = 0;  //!< length of the Ks filter
 
 /** @brief Ha filter, used to compute the coarser scaling coefficients */
 template <lda_t N, lda_t NT>
-constexpr real_t ha_[1] = {0.0};
+static constexpr real_t ha_[1] = {0.0};
 
 /** @brief S dual filter, substitutes the Ga filter and the Ks filter when details are null
  * 
@@ -26,7 +26,7 @@ constexpr real_t ha_[1] = {0.0};
  * - when the details are null, the finer scaling can also be obtained using the S_dual
  */
 template <lda_t N, lda_t NT>
-constexpr real_t ga_[1] = {0.0};
+static constexpr real_t ga_[1] = {0.0};
 
 /**
  * @brief Js filter: used to compute the EVEN scaling coefficients from the coarse value + details
@@ -34,7 +34,7 @@ constexpr real_t ga_[1] = {0.0};
  * - in practice, only needed for the smoothing, to compute the influence of a detail modification to the fine scaling
  */
 template <lda_t N, lda_t NT>
-constexpr real_t js_[1] = {0.0};
+static constexpr real_t js_[1] = {0.0};
 
 /**
  * @brief Ks filter: used to compute the ODD scaling coefficients from the coarse value + details
@@ -42,13 +42,177 @@ constexpr real_t js_[1] = {0.0};
  * - in practice, only needed for the smoothing, to compute the influence of a detail modification to the fine scaling
  */
 template <lda_t N, lda_t NT>
-constexpr real_t ks_[1] = {0.0};
+static constexpr real_t ks_[1] = {0.0};
 
 /**
  * @brief constant in front of the epsilon 
  */
 template <lda_t N, lda_t NT>
-constexpr real_t eps_c = 1.0;
+static constexpr real_t eps_c = 1.0;
+
+//-----------------------------------------------------------------------------
+// Wavelet 2.0
+template <>
+inline constexpr short_t len_ha_<2, 0> = 1;
+template <>
+inline constexpr short_t len_ga_<2, 0> = 3;
+template <>
+inline constexpr short_t len_js_<2, 0> = 1;
+template <>
+inline constexpr short_t len_ks_<2, 0> = 3;
+template <>
+inline constexpr real_t ha_<2, 0>[1] = {1.0};
+template <>
+inline constexpr real_t ga_<2, 0>[3] = {-0.5, 1.0, -0.5};
+template <>
+inline constexpr real_t js_<2, 0>[1] = {1.0};
+template <>
+inline constexpr real_t ks_<2, 0>[3] = {0.5, 1.0, 0.5};
+template <>
+inline constexpr real_t eps_c<2, 0> = 7.0;
+
+//-----------------------------------------------------------------------------
+// Wavelet 2.2
+template <>
+inline constexpr short_t len_ha_<2, 2> = 5;
+template <>
+inline constexpr short_t len_ga_<2, 2> = 3;
+template <>
+inline constexpr short_t len_js_<2, 2> = 3;
+template <>
+inline constexpr short_t len_ks_<2, 2> = 5;
+template <>
+inline constexpr real_t ha_<2, 2>[5] = {-0.125, 0.25, 0.75, 0.25, -0.125};
+template <>
+inline constexpr real_t ga_<2, 2>[3] = {-0.5, 1.0, -0.5};
+template <>
+inline constexpr real_t js_<2, 2>[3] = {-1.0 / 4.0, 1.0, -1.0 / 4.0};
+template <>
+inline constexpr real_t ks_<2, 2>[5] = {-1.0 / 8.0, 1.0 / 2.0, 3.0 / 4.0, 1.0 / 2.0, -1.0 / 8.0};
+template <>
+inline constexpr real_t eps_c<2, 2> = 7.0;
+
+//-----------------------------------------------------------------------------
+// Wavelet 4.0
+template <>
+inline constexpr short_t len_ha_<4, 0> = 1;
+template <>
+inline constexpr short_t len_ga_<4, 0> = 7;
+template <>
+inline constexpr short_t len_js_<4, 0> = 1;
+template <>
+inline constexpr short_t len_ks_<4, 0> = 7;
+template <>
+inline constexpr real_t ha_<4, 0>[1] = {1.0};
+template <>
+inline constexpr real_t ga_<4, 0>[7] = {1.0 / 16.0, 0.0, -9.0 / 16.0, 1.0, -9.0 / 16.0, 0.0, 1.0 / 16.0};
+template <>
+inline constexpr real_t js_<4, 0>[1] = {1.0};
+template <>
+inline constexpr real_t ks_<4, 0>[7] = {-1.0 / 16.0, 0.0, 9.0 / 16.0, 1.0, 9.0 / 16.0, 0.0, -1.0 / 16.0};
+template <>
+inline constexpr real_t eps_c<4, 0> = 9.4375;
+
+//-----------------------------------------------------------------------------
+// Wavelet 4.2
+template <>
+inline constexpr short_t len_ha_<4, 2> = 9;
+template <>
+inline constexpr short_t len_ga_<4, 2> = 7;
+template <>
+inline constexpr short_t len_js_<4, 2> = 3;
+template <>
+inline constexpr short_t len_ks_<4, 2> = 9;
+template <>
+inline constexpr real_t ha_<4, 2>[9] = {1.0 / 64.0, 0.0, -1.0 / 8.0, 1.0 / 4.0, 23.0 / 32.0, 1.0 / 4.0, -1.0 / 8.0, 0.0, 1.0 / 64.0};
+template <>
+inline constexpr real_t ga_<4, 2>[7] = {1.0 / 16.0, 0.0, -9.0 / 16.0, 1.0, -9.0 / 16.0, 0.0, 1.0 / 16.0};
+template <>
+inline constexpr real_t js_<4, 2>[3] = {-1.0 / 4.0, 1.0, -1.0 / 4.0};
+template <>
+inline constexpr real_t ks_<4, 2>[9] = {1.0 / 64.0, -1.0 / 16.0, -1.0 / 8.0, 9.0 / 16.0, 23.0 / 32.0, 9.0 / 16.0, -1.0 / 8.0, -1.0 / 16.0, 1.0 / 64.0};
+template <>
+inline constexpr real_t eps_c<4, 2> = 9.4375;
+
+// //-----------------------------------------------------------------------------
+// // Wavelet 4.4
+// template <>
+// constexpr short_t len_ha_<4, 4> = 13;
+// template <>
+// constexpr short_t len_ga_<4, 4> = 4;
+// template <>
+// constexpr short_t len_js_<4, 4> = 7;
+// template <>
+// constexpr short_t len_ks_<4, 4> = 13;
+// template <>
+// constexpr real_t ha_<4, 4>[13] = {-1.0 / 512.0, 0.0, 9.0 / 256.0, -1.0 / 32.0, -63.0 / 512.0, 9.0 / 32.0, 87.0 / 128.0, 9.0 / 32.0, -63.0 / 512.0, -1.0 / 32.0, 9.0 / 256.0, 0.0, -1.0 / 512.0};
+// template <>
+// constexpr real_t ga_<4, 4>[4] = {-0.0625, 0.5625, 0.5625, -0.0625};
+// template <>
+// constexpr real_t js_<4, 4>[7] = {1.0 / 32.0, 0.0, -9.0 / 32.0, 1.0, -9.0 / 32.0, 0.0, 1.0 / 32.0};
+// template <>
+// constexpr real_t ks_<4, 4>[13] = {-1.0 / 512.0, 0.0, 9.0 / 256.0, -1.0 / 16.0, -63.0 / 512.0, 9 / 16.0, 87.0 / 128.0, 9.0 / 16.0, -63.0 / 512.0, -1.0 / 16.0, 9.0 / 256.0, 0.0, -1.0 / 512.0};
+
+//-----------------------------------------------------------------------------
+// Wavelet 6.0
+template <>
+inline constexpr short_t len_ha_<6, 0> = 1;
+template <>
+inline constexpr short_t len_ga_<6, 0> = 11;
+template <>
+inline constexpr short_t len_js_<6, 0> = 1;
+template <>
+inline constexpr short_t len_ks_<6, 0> = 11;
+template <>
+inline constexpr real_t ha_<6, 0>[1] = {1.0};
+template <>
+inline constexpr real_t ga_<6, 0>[11] = {-3.0 / 256.0, 0.0, 25.0 / 256.0, 0.0, -75.0 / 128.0, 1.0, -75.0 / 128.0, 0.0, 25.0 / 256.0, 0.0, -3.0 / 256.0};
+template <>
+inline constexpr real_t js_<6, 0>[1] = {1.0};
+template <>
+inline constexpr real_t ks_<6, 0>[11] = {3.0 / 256.0, 0.0, -25.0 / 256.0, 0.0, 75.0 / 128.0, 1.0, 75.0 / 128.0, 0.0, -25.0 / 256.0, 0.0, 3.0 / 256.0};
+template <>
+inline constexpr real_t eps_c<6, 0> = 10.973388671875;
+
+//-----------------------------------------------------------------------------
+// Wavelet 6.2
+template <>
+inline constexpr short_t len_ha_<6, 2> = 13;
+template <>
+inline constexpr short_t len_ga_<6, 2> = 11;
+template <>
+inline constexpr short_t len_js_<6, 2> = 3;
+template <>
+inline constexpr short_t len_ks_<6, 2> = 13;
+template <>
+inline constexpr real_t ha_<6, 2>[13] = {-3.0 / 1024.0, 0.0, 11.0 / 512.0, 0.0, -125.0 / 1024.0, 1.0 / 4.0, 181.0 / 256.0, 1.0 / 4.0, -125.0 / 1024.0, 0.0, 11.0 / 512.0, 0.0, -3.0 / 1024.0};
+template <>
+inline constexpr real_t ga_<6, 2>[11] = {-3.0 / 256.0, 0.0, 25.0 / 256.0, 0.0, -75.0 / 128.0, 1.0, -75.0 / 128.0, 0.0, 25.0 / 256.0, 0.0, -3.0 / 256.0};
+template <>
+inline constexpr real_t js_<6, 2>[3] = {-1.0 / 4.0, 1.0, -1.0 / 4.0};
+template <>
+inline constexpr real_t ks_<6, 2>[13] = {-3.0 / 1024.0, 3.0 / 256.0, 11.0 / 512.0, -25.0 / 256.0, -125.0 / 1024.0, 75.0 / 128.0, 181.0 / 256.0, 75.0 / 128.0, -125.0 / 1024.0, -25.0 / 256.0, 11.0 / 512.0, 3.0 / 256.0, -3.0 / 1024.0};
+template <>
+inline constexpr real_t eps_c<6, 2> = 10.973388671875;
+
+// //-----------------------------------------------------------------------------
+// // Wavelet 6.4
+// template <>
+// constexpr short_t len_ha_<6, 4> = 17;
+// template <>
+// constexpr short_t len_ga_<6, 4> = 6;
+// template <>
+// constexpr short_t len_js_<6, 4> = 7;
+// template <>
+// constexpr short_t len_ks_<6, 4> = 17;
+// template <>
+// constexpr real_t ha_<6, 4>[17] = {3.0 / 8192.0, 0.0, -13.0 / 2048.0, 0.0, 87.0 / 2048.0, -1.0 / 32.0, -243.0 / 2048.0, 9.0 / 32.0, 2721.0 / 4096.0, 9.0 / 32.0, -243.0 / 2048.0, -1.0 / 32.0, 87.0 / 2048.0, 0.0, -13.0 / 2048.0, 0.0, 3.0 / 8192.0};
+// template <>
+// constexpr real_t ga_<6, 4>[6] = {3.0 / 256.0, -25.0 / 256.0, 75.0 / 128.0, 75.0 / 128.0, -25.0 / 256.0, 3.0 / 256.0};
+// template <>
+// constexpr real_t js_<6, 4>[7] = {1.0 / 32.0, 0.0, -9.0 / 32.0, 1.0, -9.0 / 32.0, 0.0, 1.0 / 32.0};
+// template <>
+// constexpr real_t ks_<6, 4>[17] = {3.0 / 8192.0, 0.0, -13.0 / 2048.0, 3.0 / 256.0, 87.0 / 2048.0, -25.0 / 256.0, -243.0 / 2048.0, 75.0 / 128.0, 2721.0 / 4096.0, 75.0 / 128.0, -243.0 / 2048.0, -25.0 / 256.0, 87.0 / 2048.0, 3.0 / 256.0, -13.0 / 2048.0, 0.0, 3.0 / 8192.0};
 
 /**
  * @brief Implement the interpolating wavelets from the familly N.Nt (with Nt <= N)
