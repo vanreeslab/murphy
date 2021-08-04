cfdbda5edbbad5117c70012b1a65870b277237e2
Thomas_Gillis <thomas.gillis@uclouvain.be>
2021-08-03 21:17:37 -0400

    :construction: add some hints on the MPI_COMM_WORLD used by the whole library

--- src/murphy.cpp
+++ src/murphy.cpp
@@ -46,14 +46,19 @@ void WriteInfo(int argc, char** argv) {
 
 TestCase* MurphyInit(int argc, char** argv) {
     //-------------------------------------------------------------------------
-    int provided;
-    // set MPI_THREAD_SERIALIZED
-    int requested = MPI_THREAD_SERIALIZED;
     MPI_Init(&argc, &argv);
     MPI_Comm comm = MPI_COMM_WORLD;
 
     sc_init(comm,0,0,NULL,SC_LP_SILENT);
     p4est_init(NULL, SC_LP_SILENT);
+
+    // set some properties on the COMM_WORLD
+    MPI_Info info;
+    MPI_Info_create(&info);
+    MPI_Info_set(info,"mpi_assert_exact_length","true");
+    MPI_Info_set(info,"mpi_assert_allow_overtaking","true");
+    MPI_Comm_set_info(MPI_COMM_WORLD,info);
+    MPI_Info_free(&info);
 
     // so dome checks for the aligment, the constants etc
     m_assert((M_N % 2) == 0, "the number of points must be odd");
