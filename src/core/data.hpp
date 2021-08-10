522ccb9a3c0207012e76e76a0f8a4fcb66b56b95
pbalty <pierre.balty@uclouvain.be>
2021-08-10 16:13:42 +0200

    :construction: Implementation of the proposed solution in issue #62

--- src/core/data.hpp
+++ src/core/data.hpp
@@ -42,12 +42,36 @@ class m_Ptr {
     }
 };
 
+
+// @ TG 
+// I think that the mem_layout should stay the base class for a '*Block' object. 
+// In most of the case, the stride and the gs of all the field contained in the block 
+// will be the same. We could then have a function sending directly the m_Data, based on 
+// the `*Block` information (since the block has the map of m_Ptr). 
+// On the other hand, we could add a m_Span structure gathering the end and the 
+// start index 
+
 struct m_Layout {
     // we could store the number of dimensions, but I would keep the gs size to 3
     // this allow us to not loop over the dimensions to compute the offset (see here-under)
     size_t stride = 0;
     size_t gs[3]  = {0, 0, 0};
 };
+
+struct m_Span { 
+    lid_t start_[3] = {0, 0, 0};  //!< starting index for the region of interest
+    lid_t end_[3]   = {0, 0, 0};  //!< ending index for the region of interest 
+    
+    /** @brief Default, parameterless constructor of the structure */
+    m_Span(){};
+
+    m_Span(lid_t start[3], lid_t end[3]){
+        for(lid_t i = 0; i < 3 ; i++){
+            start_[i] = start[i];
+            end_[i]   = end[i];
+        }
+    }
+}
 
 // defines a function that takes 3 arguments and returns an offset
 using accessor_t = std::function<bidx_t(const bidx_t, const bidx_t, const bidx_t, const bidx_t)>;
@@ -67,13 +91,16 @@ class m_Data {
         const size_t mod   = (layout.stride % chunk);
 
         // get the strides
-        stride_data_ = layou.stride;
+        stride_data_ = layout.stride;
         stride_mem_  = layout.stride - mod + (mod > 0) * chunk;
 
         // @PB if we want to check that the size of the pointer is wide enough then we need to know how many dimensions
         // is there in the layout. Why not, it can be nice for debugging purposes
+        // --> I think this information is more related to the m_Ptr class. For me, a mem_layout describes how the memory is 
+        // arranged as a grid inside the block (stride and ghostsize) while the m_Ptr has the information about the size of 
+        // the memory it owns 
 
-        // get the sifted pointer
+        // get the shifted pointer
         const size_t offset = layout.gs[0] + stride_mem_ * (layout.gs[1] + stride_data_ * layout.gs[2]);
         m_assert((offset % chunk) == 0, "the offset must be a modulo of the chunk!");
         data_ = layout.ptr + offset;
@@ -91,10 +118,19 @@ class m_Data {
     __restrict T* RWrite(const lda_t ida = 0) const {
         return data_;
     }
-    __restrict T* RRead(const lda_t ida = 0) const {
+    __restrict const T* RRead(const lda_t ida = 0) const {
         return data_;
     }
 
+    T* Write(const lda_t ida = 0 ) const { 
+        return data_
+    }
+    
+    const T* Read(const lda_t ida = 0 ) const {
+       return data_;
+    }
+
+    
     // etc...
 };
 
