b01a48f5fa3a882e588c6ab3bdcd448b6d090efe
pbalty <pierre.balty@uclouvain.be>
2021-09-16 12:17:42 +0200

    :bug: Enforce the data to 0 after the allocation

--- src/core/memdata.cpp
+++ src/core/memdata.cpp
@@ -12,6 +12,7 @@ void MemPtr::Allocate(const size_t element_size) {
     //--------------------------------------------------------------------------
     size = element_size;
     ptr  = reinterpret_cast<real_t*>(aligned_alloc(M_ALIGNMENT, element_size * sizeof(real_t)));
+    std::memset(ptr, 0, size * sizeof(real_t));
 #ifndef NDEBUG
     is_allocated = true;
 #endif
